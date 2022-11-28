b250dfe4ed247b3149b68ad3c91191bd07103e75
TatianaPovalyaeva <tanya-p79@mail.ru>
2022-11-28 23:36:04 +0300

    version1

--- src/Slide.js
+++ src/Slide.js
@@ -1,24 +1,21 @@
 import { useState } from "react";
 import { data } from "./data";
 import './App.css';
-
 function Slide(props) {
   const [card]=useState(props.numderTour-1);
   const [arrayDescr, setArrayDescr]=useState(0);
   const {image, description}=data[card]
-  const imageSet = image [arrayDescr]
+  const imageSet = image[arrayDescr]
   const descriptionSet = description[arrayDescr]
-
   const previousPhoto=()=>{
     setArrayDescr ((arrayDescr=>{
-      arrayDescr--;
+    arrayDescr--;
       if (arrayDescr<0){
         arrayDescr=image.length-1;
       }
       return arrayDescr
     }))
   }
-
   const nextPhoto=()=>{
     setArrayDescr ((arrayDescr=>{
       arrayDescr++;
@@ -33,7 +30,6 @@ function Slide(props) {
       <img src={imageSet} width="400px" alt = "tour"/>
       <h3>{descriptionSet}</h3>
       </div> 
-
       <div className="container">
       <button onClick={previousPhoto} className="btn">Назад</button> 
       <button onClick={nextPhoto} className="btn">Вперед</button>
